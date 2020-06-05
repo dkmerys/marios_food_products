43e6ade41e597e8944d762579471545518b6ec3a
Daniel Merys <dkmerys@gmail.com>
2020-06-05 08:51:53 -0700

    add user registration, sign-in/out routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root to: 'site#home'
 
+  get 'signup', to: 'users#new'
+  post '/users', to: 'users#create'
+
+  get 'signin', to: 'sessions#new'
+  post 'signin', to: 'sessions#create'
+  get '/signout', to: 'sessions#destroy'
+
   get '/home', to: 'site#home'
   resources :products do
     resources :reviews, except: [:destroy]
