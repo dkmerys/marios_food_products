f177ef8de6a86ea7031bdc6637fba227951c6ad1
Daniel Merys <dkmerys@gmail.com>
2020-06-04 21:11:22 -0700

    add landing page and minimalistic styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'products#index'
+  root to: 'site#home'
+
+  get '/home', to: 'site#home'
   resources :products do
     resources :reviews, except: [:destroy]
   end
