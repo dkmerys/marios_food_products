ee942209c7890820f6325bce6df014128a10b3e3
Daniel Merys <dkmerys@gmail.com>
2020-06-05 09:28:01 -0700

    add authorize method to app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,10 @@ class ApplicationController < ActionController::Base
       @current_user ||= User.find(session[:user_id])
     end
   end
+
+  def authorize
+    if !current_user
+      flash[:alert]
+    end
+  end
 end
