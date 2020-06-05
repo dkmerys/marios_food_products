96b66a5308b13aefe572988af6b4117dffbf524d
Daniel Merys <dkmerys@gmail.com>
2020-06-05 15:00:43 -0700

    update test for user signin

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 describe "the add a product process" do
   it "adds a new product" do
+    visit signin_path
+    fill_in 'Email', :with => "dan@mario.com"
+    fill_in 'Password', :with => "password"
+    click_on 'Sign In'
     visit products_path
     click_link 'Create new product'
     fill_in 'Name', :with => "miracle whip"
